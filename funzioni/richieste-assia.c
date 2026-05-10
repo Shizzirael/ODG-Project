d94ea62aac1b44d0507225a32bae12e23c092f4d
assia <assia.piccolo.2007@gmail.com>
2026-05-10 13:13:48 +0200

    ho aggiunto alcune nuove funzioni con anche piccoli commenti

--- funzioni/richieste-assia.c
+++ funzioni/richieste-assia.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "headers/richieste.h"
+#include "richiesta.h"
 
 static const char* statoToString(StatoRichiesta s) {
     switch (s) {
@@ -14,6 +14,24 @@ static const char* statoToString(StatoRichiesta s) {
     }
 }
  
+static int transisioneValida(StatoRichiesta attuale, StatoRichiesta nuovo) {
+    switch (attuale) {
+        case APERTA:
+            return (nuovo == PIANIFICATA || nuovo == ANNULLATA);
+        case PIANIFICATA:
+            return (nuovo == IN_LAVORAZIONE || nuovo == ANNULLATA);
+        case IN_LAVORAZIONE:
+            return (nuovo == CONCLUSA || nuovo == ANNULLATA);
+        case CONCLUSA:
+            return 0; 
+        case ANNULLATA:
+            return 0; 
+        default:
+            return 0;
+    }
+}
+ 
+
 Richiesta* creaRichiesta(int codice, const char* area, const char* tipologia,
                          const char* descrizione, const char* data, int urgenza) {
     Richiesta* nuova = (Richiesta*)malloc(sizeof(Richiesta));
@@ -37,6 +55,7 @@ Richiesta* creaRichiesta(int codice, const char* area, const char* tipologia,
     return nuova;
 }
  
+
 void inserisciRichiesta(Richiesta** testa, Richiesta* nuova) {
     if (testa == NULL || nuova == NULL) return;
  
@@ -52,11 +71,92 @@ void inserisciRichiesta(Richiesta** testa, Richiesta* nuova) {
     (*temp).next = nuova;
 }
  
-void aggiornaStato(Richiesta* r, StatoRichiesta nuovoStato) {
-    if (r == NULL) return;
+
+int aggiornaStato(Richiesta* r, StatoRichiesta nuovoStato) {
+    if (r == NULL) {
+        printf("Errore: richiesta non valida (NULL).\n");
+        return 0;
+    }
+ 
+    if (!transisioneValida((*r).stato, nuovoStato)) {
+        printf("Errore: transizione non permessa da '%s' a '%s'.\n",
+               statoToString((*r).stato),
+               statoToString(nuovoStato));
+        return 0;
+    }
+ 
     (*r).stato = nuovoStato;
+    printf("Stato aggiornato con successo: '%s'.\n", statoToString((*r).stato));
+    return 1;
 }
  
+int aggiornaStatoDaCodice(Richiesta* testa, int codice, StatoRichiesta nuovoStato) {
+    Richiesta* temp = testa;
+    while (temp != NULL) {
+        if ((*temp).codice == codice) {
+            return aggiornaStato(temp, nuovoStato);
+        }
+        temp = (*temp).next;
+    }
+    printf("Errore: nessuna richiesta trovata con codice %d.\n", codice);
+    return 0;
+}
+ 
+void menuAggiornaStato(Richiesta* testa) {
+    int codice;
+    int scelta;
+ 
+    printf("\n AGGIORNAMENTO STATO RICHIESTA \n");
+    printf("Inserisci il codice della richiesta: ");
+    scanf("%d", &codice);
+ 
+    Richiesta* r = cercaPerCodice(testa, codice);
+    if (r == NULL) {
+        printf("Richiesta con codice %d non trovata.\n", codice);
+        return;
+    }
+ 
+    printf("\nRichiesta trovata:\n");
+    stampaDettaglioRichiesta(r);
+    printf("Stato attuale: %s\n", statoToString((*r).stato));
+  // qui mostro solo le transizioni che si possono fa//
+    printf("\nSeleziona il nuovo stato:\n");
+    if ((*r).stato == APERTA) {
+        printf("  1. PIANIFICATA\n");
+        printf("  2. ANNULLATA\n");
+    } else if ((*r).stato == PIANIFICATA) {
+        printf("  1. IN LAVORAZIONE\n");
+        printf("  2. ANNULLATA\n");
+    } else if ((*r).stato == IN_LAVORAZIONE) {
+        printf("  1. CONCLUSA\n");
+        printf("  2. ANNULLATA\n");
+    } else {
+        printf("Questa richiesta e' in stato finale (%s), non modificabile.\n",
+               statoToString((*r).stato));
+        return;
+    }
+ 
+    printf("Scelta: ");
+    scanf("%d", &scelta);
+ //qua invece in base allo stato attuale applico l atransizione//
+    if ((*r).stato == APERTA) {
+        if (scelta == 1) aggiornaStato(r, PIANIFICATA);
+        else if (scelta == 2) aggiornaStato(r, ANNULLATA);
+        else printf("Scelta non valida.\n");
+ 
+    } else if ((*r).stato == PIANIFICATA) {
+        if (scelta == 1) aggiornaStato(r, IN_LAVORAZIONE);
+        else if (scelta == 2) aggiornaStato(r, ANNULLATA);
+        else printf("Scelta non valida.\n");
+ 
+    } else if ((*r).stato == IN_LAVORAZIONE) {
+        if (scelta == 1) aggiornaStato(r, CONCLUSA);
+        else if (scelta == 2) aggiornaStato(r, ANNULLATA);
+        else printf("Scelta non valida.\n");
+    }
+}
+ 
+
 Richiesta* cercaPerCodice(Richiesta* testa, int codice) {
     Richiesta* temp = testa;
     while (temp != NULL) {
@@ -66,6 +166,7 @@ Richiesta* cercaPerCodice(Richiesta* testa, int codice) {
     return NULL;
 }
  
+
 void stampaDettaglioRichiesta(Richiesta* r) {
     if (r == NULL) return;
     printf("\n");
@@ -78,7 +179,7 @@ void stampaDettaglioRichiesta(Richiesta* r) {
     printf("Stato      : %s\n",   statoToString((*r).stato));
     printf("\n");
 }
- 
+
 void stampaRichiesteFiltrate(Richiesta* testa, int filtro, int tipoFiltro) {
     Richiesta* temp = testa;
     int trovata = 0;
@@ -95,7 +196,7 @@ void stampaRichiesteFiltrate(Richiesta* testa, int filtro, int tipoFiltro) {
     }
  
     if (!trovata) {
-        printf("Nessuna richiesta trovata.\n");
+        printf("Nessuna richiesta trovata con il filtro selezionato.\n");
     }
 }
  
@@ -120,7 +221,7 @@ void stampaRichiestePerStringa(Richiesta* testa, const char* valore, int tipoFil
         printf("Nessuna richiesta trovata per il valore \"%s\".\n", valore);
     }
 }
-
+ 
 void liberaListaRichieste(Richiesta* testa) {
     Richiesta* temp;
     while (testa != NULL) {
